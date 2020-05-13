b1ee47596759c1d6904c9ec848d1b0be969f8a99
Daniel Tshibangu <45724950+danieltshibangu@users.noreply.github.com>
2020-05-12 23:05:12 -0400

    Update Galaga.py
    Added features for enemy appearance and attack. 
    Added bullets for attack feature for both me and enemy, 
    Added hit-list for deletion of enemies when hit by player, 
    Added a score count for each enemy deleted by player
    Added explosion animation for each frame,

--- Galaga.py
+++ Galaga.py
@@ -1,9 +1,11 @@
 import arcade
-import player
+from player import Player
+from Explosion import Explosion
 import os
+from arcade.draw_commands import Texture
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 SCREEN_TITLE = "GALAGA!"
 
 # defines size of the model
@@ -16,6 +18,9 @@ MOVEMENT_SPEED = 10
 SPRITE_SCALING_LASER = 0.8
 BULLET_SPEED = 10
 
+# gives explosion a texture count
+EXPLOSION_TEXTURE_COUNT = 60
+
 class MyGame( arcade.Window ):
     """
     Main application class
@@ -23,12 +28,18 @@ class MyGame( arcade.Window ):
     def __init__( self, width, height, title ):
         super().__init__(width, height, title )
         
+        # initiate the frame count here
+        self.frame_count = 0
+        
         # variables that will hold sprite lists
         self.player_list = None
         self.bullet_list = None
+        self.enemy_list = None
+        self.explosions_list = None
 
         # set up the player info
         self.player_sprite = None
+        self.score = 0
     
 
         # track the state of which key is pressed
@@ -36,6 +47,20 @@ class MyGame( arcade.Window ):
         self.right_pressed = False
         self.up_pressed = False
         self.down_pressed = False
+        
+        # preload the animation frames
+        self.explosion_texture_list = []
+
+        columns = 16
+        count = 60
+        sprite_width = 256
+        sprite_height = 256
+        file_name = ":resources:images/spritesheets/explosion.png"
+
+        # load explosions from a sprite sheet
+        self.explosion_texture_list = arcade.load_spritesheet( file_name,
+                                      sprite_width, sprite_height,
+                                      columns, count)
 
         #add gun sounds here
         """
@@ -51,9 +76,11 @@ class MyGame( arcade.Window ):
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.bullet_list = arcade.SpriteList()
+        self.enemy_list = arcade.SpriteList()
+        self.explosions_list = arcade.SpriteList() 
 
         # Set up the player
-
+        self.score = 0
         self.player_sprite = Player( r"C:\Users\Daniel Tshibangu\Desktop\galaga\Resources\airship.png" )
         # this is the starting position of the player
         self.player_sprite.center_x = 50
@@ -69,6 +96,15 @@ class MyGame( arcade.Window ):
 
         # the sprite is then appended to a list of all players
         self.player_list.append( self.player_sprite )
+        
+        # SET UP THE ENEMIES
+        enemy = arcade.Sprite( r"C:\Users\Daniel Tshibangu\Desktop\galaga\Resources\galaga-ship-png-4.png" )
+        enemy.height = 200
+        enemy.width = 200
+        enemy.center_x = 120
+        enemy.center_y = SCREEN_HEIGHT - enemy.height
+        enemy.angle = 180
+        self.enemy_list.append(enemy)
 
     def on_draw( self ):
         """
@@ -86,8 +122,24 @@ class MyGame( arcade.Window ):
         
         # draws border for hit box
         # self.player_sprite.draw_hit_box( line_thickness = 5 )
+        
+        # creates a rectangle the size of screen to contain image?
+        arcade.draw_lrwh_rectangle_textured(0, 0,
+                                            SCREEN_WIDTH, SCREEN_HEIGHT,
+                                            self.background)
                                     
+        # call draw() on all sprite lists below
+        self.player_list.draw()
+        self.enemy_list.draw()
+        self.explosions_list.draw()
+
+        # draws border for hit box
+        # self.player_sprite.draw_hit_box( line_thickness = 5 )
         self.bullet_list.draw()
+
+        # render the text for score
+        arcade.draw_text( f"Score: {self.score}", 10, 20,
+                          arcade.color.WHITE, 14 )
 
     def on_update( self, delta_time):
         """
@@ -115,14 +167,65 @@ class MyGame( arcade.Window ):
 
         # call update on bullet sprites
         self.bullet_list.update()
+        
+        # call update on explosions
+        self.explosions_list.update()
+        
+        # ENEMY MOVEMENT AND ATTACK LOGIC 
+
+        # initializing the frame count to 1
+        self.frame_count += 1
+        
+        # loop through each enemy
+        for enemy in self.enemy_list:
+
+            # Using the modules to trigger an event every 120 frames
+            if self.frame_count % 120 == 0:
+                bullet = arcade.Sprite( r"C:\Users\Daniel Tshibangu\Desktop\galaga\Resources\missle.png",
+                                     SPRITE_SCALING_LASER )
+
+                # change bullet dimensions
+                bullet.height = 200
+                bullet.width = 100
+                    
+                bullet.center_x = enemy.center_x
+                bullet.top = enemy.bottom
+                bullet.change_y = -2
+                self.bullet_list.append(bullet)
 
         # loop through each bullet
         for bullet in self.bullet_list:
 
             #Check this bullet to see if it hit something
-            """
             hit_list = arcade.check_for_collision_with_list( bullet, self.a_list )
-            """
+            
+            # if there was a collision
+            if len( hit_list ) > 0:
+
+                # make an explosion
+                explosion = Explosion( self.explosion_texture_list )
+
+                # move it to the location of the enemy
+                explosion.center_x = hit_list[0].center_x
+                explosion.center_y = hit_list[0].center_y
+
+
+                # call update() to set starting explosion frame
+                explosion.update()
+
+                # add a list of sprite to that explosion
+                self.explosions_list.append( explosion )
+
+                # get rid of the bullet
+                bullet.remove_from_sprite_lists()
+                
+            # for each enemy we hit, add to score andremove enemy
+            for enemy in self.enemy_list:
+                if enemy in hit_list:
+                    enemy.remove_from_sprite_lists()
+                    bullet.remove_from_sprite_lists()
+                    self.score += 1
+                
 
             # if the bullet flies of the screen it must be removed
             if bullet.bottom > SCREEN_HEIGHT:
@@ -150,7 +253,7 @@ class MyGame( arcade.Window ):
             bullet = arcade.Sprite( r"C:\Users\Daniel Tshibangu\Desktop\galaga\Resources\missle.png",
                                      SPRITE_SCALING_LASER )
 
-            # chnaged the size of the bullet
+            # changed the size of the bullet
             bullet.height = 200
             bullet.width = 100
 
@@ -158,8 +261,8 @@ class MyGame( arcade.Window ):
             bullet.change_y = BULLET_SPEED
 
             # position of the bullet
-            bullet.center_x = self.player_sprite.center_x - 9
-            bullet.bottom = self.player_sprite.top
+            bullet.center_x = self.player_sprite.center_x 
+            bullet.bottom = self.player_sprite.top - 2
 
             # add the bullet to it's list once more
             self.bullet_list.append(bullet)
@@ -176,24 +279,6 @@ class MyGame( arcade.Window ):
             self.left_pressed = False
         elif key == arcade.key.RIGHT:
             self.right_pressed = False
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
 
 
 def main():
